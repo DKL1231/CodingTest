a50cf545ea2a7f658671b09efa06391be21d309c
DKL <95444292+DKL1231@users.noreply.github.com>
2022-10-30 10:57:48 +0900

    [Silver V] Title: LCM, Time: 68 ms, Memory: 32952 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.py
@@ -0,0 +1,10 @@
+import sys
+import math
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    gcd = math.gcd(a, b)
+
+    print(int(a * b / gcd))
