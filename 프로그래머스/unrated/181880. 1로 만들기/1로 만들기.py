628ab84a177b49e583890ed9a0fafdd8332bbadf
DKL <95444292+DKL1231@users.noreply.github.com>
2023-09-26 17:39:43 +0900

    [unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
@@ -1,17 +1,7 @@
 def solution(num_list):
     dp = [0]*31
-    dp[2] = 1
-    for i in range(3, 31):
-        if i%2:
-            dp[i] = dp[i-1]
-        else:
-            tmp = i
-            while tmp != 1:
-                for j in range(1, i):
-                    if not tmp%(i-j):
-                        tmp //= (i-j)
-                        dp[i] += dp[i-j]
-                        break
+    for i in range(2, 31):
+        dp[i] = dp[i//2]+1
     answer = 0
     for num in num_list:
         answer += dp[num]
