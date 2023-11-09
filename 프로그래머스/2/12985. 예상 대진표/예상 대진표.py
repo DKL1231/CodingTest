b69de4a295bbd7db7d385acaf845e2653471201b
DKL <95444292+DKL1231@users.noreply.github.com>
2023-11-09 13:22:54 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,15 +1,11 @@
 def solution(n,a,b):
-    tmp = n//2
     if a%2: a+=1
     if b%2: b+=1
-    ta, tb = True if a<=tmp else False, True if b<=tmp else False
     answer = 1
     while a != b:
         a //= 2
         b //= 2
         if a%2: a+=1
         if b%2: b+=1
-        ta, tb = True if a<=tmp else False, True if b<=tmp else False
-        tmp //= 2
         answer += 1
     return answer
