897f04326c3d05be621957acdb97d92887ab077f
DKL <95444292+DKL1231@users.noreply.github.com>
2024-02-14 13:55:48 +0900

    [Silver I] Title: Z, Time: 76 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,35 +1,34 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 public class Main {
-	static int r;
-	static int c;
-	public static void main(String[] args) throws java.io.IOException {
+	static int N, r, c;
+	static int dfs(int size, int x, int y) {
+		if(x == 0 && y == 0) return 0;
+		int answer = 0;
+		if(x<size/2&&y<size/2) {
+			answer += dfs(size/2, x, y);
+		}else if(x<size/2 && y >= size/2) {
+			answer += Math.pow(size/2, 2);
+			answer += dfs(size/2, x, y-size/2);
+		}else if(x>=size/2 && y <size/2) {
+			answer += Math.pow(size/2, 2)*2;
+			answer += dfs(size/2, x-size/2, y);
+		}else {
+			answer += Math.pow(size/2, 2)*3;
+			answer += dfs(size/2, x-size/2, y-size/2);
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int n=Integer.parseInt(st.nextToken());
-		r=Integer.parseInt(st.nextToken());	
-		c=Integer.parseInt(st.nextToken());	
-		System.out.println(solve(0, 0, (int)Math.pow(2, n)));
-	}
-	static int solve(int row, int col, int dim) {
-		if(row == r && col == c)
-			return 0;
-		int sum = 0;
-		if(row <= r && r < row+dim/2 && col <= c && c < col+dim/2) {
-			sum += solve(row, col, dim/2);
-		}
-		else if (row <= r && r < row+dim/2 && col+dim/2 <= c && c < col+dim) {
-			sum += Math.pow(dim/2, 2);
-			sum += solve(row, col+dim/2, dim/2);
-		}
-		else if (row+dim/2 <= r && r < row+dim && col <= c && c < col+dim/2) {
-			sum += Math.pow(dim/2, 2)*2;
-			sum += solve(row+dim/2, col, dim/2);
-		}
-		else {
-			sum += Math.pow(dim/2, 2)*3;
-			sum += solve(row+dim/2, col+dim/2, dim/2);
-		}
-		return sum;
+		
+		N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		System.out.println(dfs((int)Math.pow(2, N), r, c));
 	}
 }
