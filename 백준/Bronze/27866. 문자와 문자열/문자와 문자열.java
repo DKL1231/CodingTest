a763473364b9bef1fc9d8eec07ec3e266f6b03f6
DKL <95444292+DKL1231@users.noreply.github.com>
2024-02-21 09:15:32 +0900

    [Bronze V] Title: 문자와 문자열, Time: 76 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().charAt(Integer.parseInt(br.readLine())-1));
+	}
+
+}
