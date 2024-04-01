2a901b504046b6c29e41ad35337831f0d8aef01d
DKL <95444292+DKL1231@users.noreply.github.com>
2024-04-01 17:14:26 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 1968 ms, Memory: 88188 KB -BaekjoonHub

---
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final int mod = 1_000_000_007;
+	static int t;
+	static long fact[] = new long[4000001];
+	
+	static long pow(long v, long idx) {
+		long result = 1;
+		while(idx>0) {
+			if(idx%2 == 1) {
+				result *= v;
+				result %= mod;
+			}
+			v *= v;
+			v %= mod;
+			idx /= 2;
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		t = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		fact[0] = 1;
+		fact[1] = 1;
+		for(int i = 2; i<=4000000; i++) {
+			fact[i] = (fact[i-1]*i)%mod;
+		}
+		
+		for(int tc = 1; tc<=t; tc++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int n = Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken());
+			long result = fact[n];
+			result *= pow((fact[r]*fact[n-r])%mod, mod-2)%mod;
+			result %= mod;
+			System.out.println(result);
+		}
+	}
+}
