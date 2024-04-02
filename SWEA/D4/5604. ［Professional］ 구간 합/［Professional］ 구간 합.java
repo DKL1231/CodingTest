5fa30355a987827a9ca2d2808c63d8e87164834d
DKL <95444292+DKL1231@users.noreply.github.com>
2024-04-02 20:01:59 +0900

    [D4] Title: [Professional] 구간 합, Time: 109 ms, Memory: 24,680 KB -BaekjoonHub

---
+++ SWEA/D4/5604. ［Professional］ 구간 합/［Professional］ 구간 합.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static long[] result;
+
+	static void count(long n, long dig, boolean type) {
+		while (n > 0) {
+			long tmp = n-(n/10*10);
+			if(type) 
+				result[(int) tmp] += dig;
+			else
+				result[(int) tmp] -= dig;
+			n /= 10;
+		}
+	}
+
+	static void solve(long from, long to, long dig, boolean type) {
+		while(from%10 != 0 && from<=to) {
+			count(from, dig, type);
+			from++;
+		}
+		
+		if(from>to) return;
+		
+		while(to%10 != 9 && from<=to) {
+			count(to, dig, type);
+			to--;
+		}
+		
+		long tmp = to/10-from/10+1;
+		for(int i = 0; i<10; i++)
+			if(type)
+				result[i]+=tmp*dig;
+			else
+				result[i]-=tmp*dig;
+		
+		solve(from/10, to/10, dig*10, type);
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= t; tc++) {
+			result = new long[10];
+			st = new StringTokenizer(br.readLine(), " ");
+			long answer = 0;
+			long n = Long.parseLong(st.nextToken());
+			long m = Long.parseLong(st.nextToken());
+			
+			solve(1, n-1, 1, false);
+			solve(1, m, 1, true);
+			for(int i = 0; i<10; i++)
+				answer += result[i]*i;
+			System.out.println("#"+tc+" "+answer);
+		}
+	}
+}
