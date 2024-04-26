2dc3c16f60eafab1820d1ec9dcc31d0e929fe159
DKL <95444292+DKL1231@users.noreply.github.com>
2024-04-26 09:55:36 +0900

    [Gold IV] Title: 단어 수학, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -7,41 +9,19 @@ public class Main {
 	static BufferedReader br;
 	static StringTokenizer st;
 	static String[] words;
-	static boolean[] charUsed = new boolean[26];
 	static int[] charValue = new int[26];
-	static char[] chars;
-	static boolean[] visit = new boolean[10];
 	static int answer = 0;
-
-	static void perm(int depth) {
-		if (depth == chars.length) {
-			calc();
-			return;
-		}
-
-		for (int i = 9; i >= 10 - chars.length; i--) {
-			if (!visit[i]) {
-				visit[i] = true;
-				charValue[chars[depth] - 'A'] = i;
-				perm(depth + 1);
-				visit[i] = false;
-			}
-		}
-	}
-
+	
 	static void calc() {
-		int value = 0;
-
-		for (String word : words) {
-			int tmp = (int) Math.pow(10, word.length() - 1);
-			for (char c : word.toCharArray()) {
-				value += tmp * charValue[c - 'A'];
-				tmp /= 10;
-			}
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		int tmp = 9;
+		for(int i = 0; i<26; i++) {
+			pq.add(charValue[i]);
 		}
-
-		if (value > answer)
-			answer = value;
+		
+		while(!pq.isEmpty()) {
+			answer += pq.poll()*tmp--;
+		}
 	}
 
 	static void input() throws Exception {
@@ -51,25 +31,20 @@ public class Main {
 
 		words = new String[n];
 
-		int idx = 0;
 		for (int i = 0; i < n; i++) {
 			words[i] = br.readLine();
-			for (char c : words[i].toCharArray()) {
-				if (!charUsed[c - 'A']) {
-					charUsed[c - 'A'] = true;
-					idx++;
-				}
+			StringBuilder sb = new StringBuilder(words[i]);
+			int tmp = 1;
+			for (char c : sb.reverse().toString().toCharArray()) {
+				charValue[c-'A'] += tmp;
+				tmp *= 10;
 			}
 		}
-		chars = new char[idx];
-		for (int i = 0, j = 0; i < 26; i++)
-			if (charUsed[i])
-				chars[j++] = (char) (i + 'A');
 	}
 
 	public static void main(String[] args) throws Exception {
 		input();
-		perm(0);
+		calc();
 		System.out.println(answer);
 	}
 }
